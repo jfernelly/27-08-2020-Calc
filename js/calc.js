c8a1b7d095e207aed5e9ad05c969590ba14ae79c
Jose Fernelly <jfernelly@gmail.com>
2020-09-09 09:37:48 -0500

    se crea calculadora con campo select

--- js/calc.js
+++ js/calc.js
@@ -19,7 +19,40 @@ promedio.addEventListener("click", ()=>{
     //ingtesarla a un campo
     //capturar daros desde un campo
     //capturar dato desde un boton
+})
+
+//PARA CALCULADORA
+
+const botonIgual = document.getElementById("botonIgual")
+botonIgual.addEventListener("click", ()=> {
+    let numero1 = parseFloat(document.getElementById("numero1").value);
+    let numero2 = parseFloat(document.getElementById("numero2").value);
+   
+    let ope = document.getElementById("ope").value;      
+    console.log(numero1);
+    console.log(numero2);
+    console.log(ope);
 
 
+    switch (ope) {
+        case "+": 
+            resultado = numero1 + numero2
+            break;
+
+        case "-": 
+            resultado = numero1 - numero2
+            break;
+        
+        case "*": 
+            resultado = numero1 * numero2
+            break;
+        
+        default:
+            resultado = numero1 / numero2
+            break;
+    }
+    let numeroResultado = parseFloat(document.getElementById("resultado").value= resultado);
 
 })
+
+
